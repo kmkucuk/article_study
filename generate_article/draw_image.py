05b37d1ff6e5a0174d45f0b3b00867cc5cfafe81
Jamario Rankins <jamariorankins@gmai.com>
2024-01-10 19:10:35 -0500

    Add auhtor to new line

--- generate_article/draw_image.py
+++ generate_article/draw_image.py
@@ -1,11 +1,6 @@
 from wand.image import Image
 
-from generate_article import (
-    ArticleInfo,
-    roundPosition,
-    page_indent_horizontal,
-    center_text,
-)
+from generate_article import ArticleInfo, roundPosition, center_text, eval_metrics
 
 from generate_article.draw_robot import indent_from_robot, robot_start
 
@@ -36,36 +31,49 @@ def draw_image(image, draw, article_info: ArticleInfo, y_position) -> list[int]:
             top=photo_position[1],
         )
 
-    caption_position_y = roundPosition(photo_position[1] * 1.15 + photo_height)
+    caption_position_y = roundPosition(photo_position[1] * 1.16 + photo_height)
 
     draw.line(
-        (robot_start * 1.8, caption_position_y - 20),
-        (image.width - robot_start * 1.8, caption_position_y - 20),
-    )
-
-    draw.line(
-        (robot_start * 1.8, caption_position_y + 15),
-        (image.width - robot_start * 1.8, caption_position_y + 15),
+        (robot_start * 1.8, caption_position_y - 25),
+        (image.width - robot_start * 1.8, caption_position_y - 25),
     )
 
     draw(image)
+
+    caption: str = article_sheet["content"][captionIndex]
+
+    caption, author = caption[: caption.index("(")], caption[caption.index("(") :]
 
     draw.font_size = 20
     draw.font_weight = article_sheet["weight"][captionIndex]
     draw.font_style = article_sheet["style"][captionIndex]
     draw.fill_color = article_sheet["color"][captionIndex]
 
-    caption_position_x = roundPosition(
-        center_text(draw, image, article_sheet["content"][captionIndex])
-    )
-
-    if caption_position_x < 0:
-        caption_position_x = 0
+    caption_position_x = roundPosition(center_text(draw, image, caption))
 
     draw.text(
         caption_position_x,
         caption_position_y,
-        article_sheet["content"][captionIndex],
+        caption,
     )
 
-    return [caption_position_x, caption_position_y]
+    caption_metrics = eval_metrics(caption, draw, image)
+    draw.font_size = 18
+
+    author_position_x = roundPosition(center_text(draw, image, author))
+    author_position_y = roundPosition(caption_position_y + caption_metrics[1])
+
+    draw.text(
+        roundPosition(center_text(draw, image, author)),
+        roundPosition(caption_position_y + caption_metrics[1]),
+        author,
+    )
+
+    author_metrics = eval_metrics(author, draw, image)
+
+    draw.line(
+        (robot_start * 1.8, author_position_y + 15),
+        (image.width - robot_start * 1.8, author_position_y + 15),
+    )
+
+    return [author_position_x, author_position_y]
