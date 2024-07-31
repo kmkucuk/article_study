63b03b0ee975e44b75f2f54ebc139e4ca312e677
Jamario Rankins <jamariorankins@gmai.com>
2024-07-31 17:52:58 -0400

    Add caption

--- generate_article/draw_image.py
+++ generate_article/draw_image.py
@@ -1,6 +1,13 @@
 from wand.image import Image
 
-from generate_article import ArticleInfo, roundPosition, center_text, eval_metrics
+from generate_article import (
+    ArticleInfo,
+    roundPosition,
+    center_text,
+    eval_metrics,
+    page_indent_horizontal,
+    img_width,
+)
 
 from generate_article.draw_robot import indent_from_robot, robot_start
 
@@ -15,66 +22,38 @@ def draw_image(image, draw, article_info: ArticleInfo, y_position: float) -> lis
     captionIndex = article_info.captionIndex
 
     photo_position = [
-        roundPosition(image.width * 0.5),
+        page_indent_horizontal,
         roundPosition(y_position),
     ]
 
-    photo_height = 0
-
-    img_width = 600
     with Image(filename=article_sheet["content"][photoIndex]) as photo:
-        photo.resize(int(img_width * 1.8), img_width)
+        photo.resize(int(img_width * 0.75), 600)
 
-        photo_height = photo.height
         image.composite(
             photo,
-            left=int(photo_position[0] - photo.width / 2),
+            # left=int(photo_position[0] - photo.width / 2),
+            left=photo_position[0],
             top=photo_position[1],
         )
 
-    caption_position_y = roundPosition(photo_position[1] * 1.16 + photo_height)
-
-    draw.line(
-        (robot_start * 1.8, caption_position_y - 25),
-        (image.width - robot_start * 1.8, caption_position_y - 25),
-    )
+        caption_position_y = roundPosition(photo_position[1] * 1.16 + photo.height)
 
     draw(image)
 
     caption: str = article_sheet["content"][captionIndex]
 
-    caption, author = caption[: caption.index("(")], caption[caption.index("(") :]
-
-    draw.font_size = 20
+    draw.font_size = 15
     draw.font_weight = article_sheet["weight"][captionIndex]
     draw.font_style = article_sheet["style"][captionIndex]
     draw.fill_color = article_sheet["color"][captionIndex]
-
-    caption_position_x = roundPosition(center_text(draw, image, caption))
+    # exit(0)
 
     draw.text(
-        caption_position_x,
+        page_indent_horizontal,
         caption_position_y,
         caption,
     )
 
     caption_metrics = eval_metrics(caption, draw, image)
-    draw.font_size = 18
 
-    author_position_x = roundPosition(center_text(draw, image, author))
-    author_position_y = roundPosition(caption_position_y + caption_metrics[1])
-
-    draw.text(
-        roundPosition(center_text(draw, image, author)),
-        roundPosition(caption_position_y + caption_metrics[1]),
-        author,
-    )
-
-    author_metrics = eval_metrics(author, draw, image)
-
-    draw.line(
-        (robot_start * 1.8, author_position_y + 15),
-        (image.width - robot_start * 1.8, author_position_y + 15),
-    )
-
-    return [author_position_x, author_position_y]
+    return [page_indent_horizontal, caption_position_y]
